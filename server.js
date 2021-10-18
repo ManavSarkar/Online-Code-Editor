b7078988e23fa2b14ef2a4f09ec52995db2ea9fd
Manav Sarkar <manavsarkar07@gmail.com>
2021-10-18 07:03:00 +0000

    irrelevant files

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
