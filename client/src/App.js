9749a71e2e9c0068b3363687875e34196da47652
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2023-03-20 22:13:51 +0530

    add react client

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { render } from "react-dom";
+
+import EditorScreen from './screens/editorScreen';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <EditorScreen />
+
+    </div>
+  );
+}
+
+export default App;
